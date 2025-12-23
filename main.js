001c8ec36be36e76ecf4cb726ec26641841fd0d7
vitorariel145-star <vitorariel145@gmail.com>
2025-12-23 00:51:24 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,22 +1,27 @@
-let vendidos = 0;
-let renda = 0;
+let vendidos = localStorage.getItem("vendidos");
+let renda = localStorage.getItem("renda");
 
-if (localStorage.getItem("vendidos")) {
-  vendidos = Number(localStorage.getItem("vendidos"));
-}
+vendidos = vendidos ? Number(vendidos) : 0;
+renda = renda ? Number(renda) : 0;
 
-if (localStorage.getItem("renda")) {
-  renda = Number(localStorage.getItem("renda"));
-}
+let valorSelecionado = 0;
 
 function atualizarTela() {
-  document.getElementById("vendidos").innerText = vendidos;
-  document.getElementById("renda").innerText = renda;
+  document.getElementById("vendidos").textContent = vendidos;
+  document.getElementById("renda").textContent = renda;
 }
 
-function comprar(valor) {
+function selecionar(valor) {
+  valorSelecionado = valor;
+  document.getElementById("valorEscolhido").textContent = valor;
+  document.getElementById("resumo").style.display = "block";
+}
+
+function comprar() {
+  if (!valorSelecionado) return;
+
   vendidos++;
-  renda += valor;
+  renda += valorSelecionado;
 
   localStorage.setItem("vendidos", vendidos);
   localStorage.setItem("renda", renda);
